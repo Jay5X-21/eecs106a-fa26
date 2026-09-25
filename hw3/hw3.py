ad641c0ca6e0aee8e2c224a367a3269b4756d104
Jaehyun Lim <jaylim@berkeley.edu>
2026-09-24 18:20:01 -0700

    .

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -127,7 +127,7 @@ def fk_2(theta):
     # Specify all twists.
     xi_1 = [0, 0, 2, 0, 0, 1]
     xi_2 = [-10, 0, 0, 0, 1, 0]
-    xi_3 = [5, 0, 0, 0, -1, 0]
+    xi_3 = [0, 5, -5, 1, 0, 0]
 
     # Specify end effector configuration at theta = 0.
     gst0 = np.array([[0, -1, 0, 0],
